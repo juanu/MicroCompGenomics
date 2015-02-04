08bec38b3ed4e0a27dc696b81ec9c443a2163758
Juan Ugalde <juanuu@gmail.com>
2015-02-04 01:07:54 -0300

    Added Summarize script

--- Summarize_FastOrthoResults.py
+++ Summarize_FastOrthoResults.py
@@ -76,7 +76,165 @@ def parse_fastortho(cf, gl):
             continue
 
         for protein in clean_protein_list:
-            cluster_dictionary[ids_proteins[0]].append(protein)
+            cluster_id = ids_proteins[0].split(" (")[0]
+
+            cluster_dictionary[cluster_id].append(protein)
             proteins_in_cluster.add(protein)
 
     return cluster_dictionary, proteins_in_cluster, unique_proteins_genome_count, total_cluster_count, clusters_removed
+
+
+def read_group_files(gf):
+    """
+    Reads a file with the group list and returns a dictionary containing the name of the group
+    and a list of the genomes that are present in that group.
+    The input file requires that the first column is the prefix of the genome and the second the name
+    of the group
+    :param gf: Group file
+    :return: Genome groups
+    """
+
+    from collections import defaultdict
+
+    genome_groups = defaultdict(list)
+
+    for line in open(gf, 'r'):
+        line = line.rstrip()
+        element = line.split("\t")
+
+        genome_groups[element[0]].append(element[1])
+
+    return genome_groups
+
+
+def get_unique_seqs_genome(protein_genomes, protein_clusters, protein_length, min_length):
+    """
+    This module takes a dictionary with their genome and proteins, and a set of proteins
+    and look for proteins that are not in the total set
+    """
+
+    from collections import defaultdict
+
+    large_unique_sequences = defaultdict(list)
+    short_unique_sequences = defaultdict(list)
+    processed_sequences = 0
+
+    for genome in protein_genomes:
+        for seq_name in protein_genomes[genome]:
+            processed_sequences += 1
+
+            if seq_name in protein_clusters:
+                continue
+            else:
+                if protein_length[seq_name] > min_length:
+                    large_unique_sequences[genome].append(seq_name)
+                else:
+                    short_unique_sequences[genome].append(seq_name)
+
+    return large_unique_sequences, short_unique_sequences, processed_sequences
+
+
+def seqs_shared_clusters(cd, gd):
+    """
+    :param cd: Cluster dictionary
+    :param gd: Genome dictionary
+    :return: Genome groups
+    """
+
+    unique_clusters = defaultdict(list)  # Dictionary with the unique clusters
+    shared_single_clusters = []  # List with clusters that are shared and single copy
+    shared_multiple_clusters = []  # List with clusters that are shared and in multiple copies
+
+    genomes_in_matrix = sorted(gd.keys())
+    header = ["Cluster_ID"]
+    header.extend(sorted(gd.keys()))
+    all_clusters_matrix = [header]
+
+    for cluster in cd:
+
+        genome_list = [protein.split("|")[0] for protein in cd[cluster]]  # Create a list with the genomes
+
+        count = {x: genome_list.count(x) for x in genome_list}  # Count the occurences
+
+        #Create the matrix
+        cluster_matrix = [cluster]
+
+        for genome in genomes_in_matrix:
+
+            if genome in genome_list:
+                cluster_matrix.append(count[genome])
+            else:
+                cluster_matrix.append(0)
+
+        #print cluster_matrix
+        all_clusters_matrix.append(cluster_matrix)
+
+        if len(count) == 1:
+            unique_clusters[genome_list[0]].append(cluster)
+
+        elif len(count) == len(gd.keys()):
+            if sum(count.itervalues()) == len(gd.keys()):
+                shared_single_clusters.append(cluster)
+            else:
+                shared_multiple_clusters.append(cluster)
+
+    return unique_clusters, shared_single_clusters, shared_multiple_clusters, all_clusters_matrix
+
+
+def clusters_in_groups(clusters, groups):
+    """
+
+    """
+    from collections import defaultdict
+
+    import itertools
+    unique_group_clusters = defaultdict(list)  # Count the unique clusters in each group
+
+    combination_clusters = defaultdict(list)
+
+    #Create inverted dictionary
+    genome_group_info = defaultdict()
+
+    for group in groups:
+        for genome in groups[group]:
+            genome_group_info[genome] = group
+
+    for cluster in clusters:
+
+        group_count = defaultdict(lambda: defaultdict(int))
+
+        for protein in clusters[cluster]:
+            genome_id = protein.split("|")[0]
+            group_for_genome = genome_group_info[genome_id]
+
+            group_count[group_for_genome][genome_id] += 1
+
+        ##Unique clusters for each group
+
+        if len(group_count) == 1:
+            for group in group_count:
+                if len(group_count[group]) == len(groups[group]):
+                    unique_group_clusters[group].append(cluster)
+
+                else:  # I could add something here to count the number of proteins not unique
+                    pass
+
+        #Shared, all possible combinations
+        else:
+            for combination_count in range(2, len(group_count) + 1):
+                for group_combinations in itertools.combinations(groups.keys(), combination_count):
+
+                    # Check that all the genomes in the group are represented
+                    group_check = 0
+
+                    for i in range(0, len(group_combinations)):
+                        if len(group_count[group_combinations[i]]) == len(groups[group_combinations[i]]):
+                            continue
+                        else:
+                            group_check = 1
+
+                    if group_check == 0:
+                        combination_clusters[group_combinations].append(cluster)
+
+    return unique_group_clusters, combination_clusters
+
