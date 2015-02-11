8781dadc3227f0e822a0f63732392e12c372c372
Juan Ugalde <juanuu@gmail.com>
2015-02-11 20:45:02 -0300

    Added annotation scripts

--- RunTribeMCL.py
+++ RunTribeMCL.py
@@ -67,7 +67,10 @@ else:
     while i < 5.1:
 
         output_file = args.output_directory + "/out.seq.mci.I" + str(i)
-        run_mcl = "mcl" + mci_file + " -I " + str(i) + " -te " + str(threads) + " -o " + output_file + " -use-tab " + tab_file
+        run_mcl = "mcl" + mci_file + " -I " + str(i) + " -te " + str(threads) + \
+                  " -o " + output_file + " -use-tab " + tab_file
+
+        os.system(run_mcl)
 
         #Count number of clusters
         number_cluster = 0
